d9870b0b7dfcc0c4f84f6ce0ed2b868747a826f8
Keenuts <nathan.gauer@lse.epita.fr>
2017-05-05 13:47:36 +0200

    chars: can print some chars now
    Signed-off-by: Keenuts <nathan.gauer@lse.epita.fr>

--- led.c
+++ led.c
@@ -17,10 +17,13 @@
 #define RS DATA_2
 #define RW DATA_3
 #define E DATA_4
-#define ACK_DELAY 200
+#define ACK_DELAY 2
 
 
 void zero_pins() {
+	digitalWrite(RS, 0);
+	digitalWrite(RW, 0);
+	digitalWrite(E, 0);
 	for (uint32_t i = 0; i < 10; i++)
 		digitalWrite(DATA_2 + i, 0);
 }
@@ -31,17 +34,18 @@ void pulse() {
 	digitalWrite(E, 1);
 	_delay_ms(1);
 	digitalWrite(E, 0);
-	_delay_ms(200);
+	_delay_ms(ACK_DELAY);
+	zero_pins();
 }
 
-void reset() {
+void clear() {
 	zero_pins();
 	digitalWrite(DATA0, 1);
+
 	pulse();
-	zero_pins();
 }
 
-void set_screen_status(int on, int cursor, int blink) {
+static void set_screen_status(int on, int cursor, int blink) {
 	zero_pins();
 
 	digitalWrite(RS, 0);
@@ -51,13 +55,28 @@ void set_screen_status(int on, int cursor, int blink) {
 	digitalWrite(DATA1, cursor);
 	digitalWrite(DATA2, on);
 	digitalWrite(DATA3, 1);
-	digitalWrite(DATA4, 0);
-	digitalWrite(DATA5, 0);
-	digitalWrite(DATA6, 0);
-	digitalWrite(DATA7, 0);
 
 	pulse();
+}
+
+void function_set(uint8_t multiline, uint8_t big_font) {
 	zero_pins();
+		
+	digitalWrite(DATA2, big_font);
+	digitalWrite(DATA3, multiline);
+	digitalWrite(DATA4, 1);
+	digitalWrite(DATA5, 1);
+	pulse();
+}
+
+void write_char(uint8_t c) {
+	zero_pins();
+	digitalWrite(RS, 1); 
+
+	for (uint8_t i = 0; i < 8; i++, c = c >> 1)
+		digitalWrite(DATA0 + i, c & 1);
+	
+	pulse();
 }
 
 static void setup()
@@ -70,28 +89,40 @@ static void setup()
 	setDirection(DATA5, OUT);
 	setDirection(DATA6, OUT);
 	setDirection(DATA7, OUT);
-
 	setDirection(E, OUT);
 	setDirection(RS, OUT);
 	setDirection(RW, OUT);
 
-	//_delay_ms(ACK_DELAY);
+	_delay_ms(ACK_DELAY);
 }
 
-static void loop(void)
-{
-	//_delay_ms(BLINK_DELAY_MS);
+#define DDRAM 1
+#define CGRAM 0
+
+void select_memory_area(uint8_t area) {
+	zero_pins();
+	if (area == DDRAM)
+		digitalWrite(DATA7, 1);
+	else
+		digitalWrite(DATA6, 1);
+	pulse();
 }
 
 int main (void)
 {
 	setup();
-	reset();
+	set_screen_status(1, 1, 1);
+	clear();
+	_delay_ms(1000);
+	for (int i = 0; i < 60; i++)
+		write_char(40);
+	//select_memory_area(DDRAM);
+	//clear();
+
+	const char* str = "abcdefghijkl";
+	for (int i = 0; i < 12; i++)
+		write_char(str[i]); 
+
+	_delay_ms(5000);
 	set_screen_status(0, 0, 0);
-	set_screen_status(1, 0, 0);
-	set_screen_status(1, 1, 0);
-
-
-	while(1)
-		loop();
 }
