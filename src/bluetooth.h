2f576879503299457cb62441bd78aef7dbc9da63
Nathan Gauer <nathan.gauer@gmail.com>
2017-12-11 22:10:21 +0100

    cleanup: removed unused files and added small comment
    Signed-off-by: Nathan Gauer <nathan.gauer@gmail.com>

--- src/bluetooth.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define TX DATA_2
-#define RX DATA_3
-
-void initialize(void)
-{
-}
--- src/lcd.h
+++ src/lcd.h
@@ -5,6 +5,8 @@
 #define READ 0
 #define WRITE 1
 
+/* This file provides simple functions to drive a basic 2x8 Ascii display. */
+
 void clear();
 void write_char(uint8_t c);
 void set_mode(uint8_t mode);
